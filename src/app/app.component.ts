d57e778a8cdd7829a48b5b1fc24994526311fb5d
Elmar Amanov <Elmar.amanov10@gmail.com>
2021-04-10 16:01:26 +0400

    Angular TodoList

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
+export class Todo{
+  constructor(
+    public name?: string,
+    public sname?: string){}
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'iputAddToTable';
+  item: Todo = new Todo();
+  data: Todo[] = [];
+  saveData(){
+    console.log(this.item)
+    this.data.push({...this.item})
+    this.item.name ="";
+    this.item.sname ="";
+  }
 }
